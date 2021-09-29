07db89ff0ec64f1dfb5fac8dfaf265aed9ab8536
song0180 <song0180@e.ntu.edu.sg>
2021-09-29 23:05:22 +0800

    Add Games page

--- frontend/src/pages/GamesPage/index.js
+++ frontend/src/pages/GamesPage/index.js
@@ -1,16 +1,47 @@
 import * as React from 'react';
-import { Input } from 'antd';
+import { Input, List, Skeleton } from 'antd';
 
 import './index.css';
 import { SearchOutlined } from '@ant-design/icons';
+import { useGameStore } from '../../services/zustand/game';
+import { GameCard } from '../../components';
 
 const GamesPage = () => {
+  const { isLoading, games, fetchGames } = useGameStore();
   const [searchStr, setSearchStr] = React.useState('');
+  const [filteredGames, setFilteredGames] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchGames();
+  }, [fetchGames]);
+
+  React.useEffect(() => {
+    setFilteredGames(games);
+  }, [games]);
+
+  const handleOnSearch = React.useCallback(
+    (searchValue) => {
+      const lowercaseSearchValue = searchValue.toLowerCase();
+      // filter games based on search string
+      const updatedFilteredGames = games.filter((game) =>
+        game.title.toLowerCase().includes(lowercaseSearchValue)
+      );
+      setFilteredGames(updatedFilteredGames);
+    },
+    [games]
+  );
+
+  React.useEffect(() => {
+    // display all games when search string is 0
+    if (searchStr.length === 0) {
+      handleOnSearch(searchStr);
+    }
+  }, [handleOnSearch, searchStr]);
 
   return (
-    <div className='container'>
-      <div className='header-container'>
-        <h2 className='heading'>Game Time!</h2>
+    <div className='game-page-container'>
+      <div className='game-page-header-container'>
+        <h2 className='game-page-heading'>Game Time!</h2>
         <Input
           prefix={
             <SearchOutlined style={{ color: 'rgba(17, 17, 17, 0.48)' }} />
@@ -21,7 +52,7 @@ const GamesPage = () => {
           value={searchStr}
           onChange={(e) => setSearchStr(e.target.value)}
           onPressEnter={() => {
-            console.log(searchStr);
+            handleOnSearch(searchStr);
           }}
         />
       </div>
@@ -30,7 +61,27 @@ const GamesPage = () => {
           Hi <span className='text-highlight'>James</span>, Please select the
           game you want tot start playing!
         </p>
-        <div className='card-container'></div>
+        <div className='games-container'>
+          <List
+            loading={isLoading}
+            grid={{
+              gutter: [30, 16],
+              column: filteredGames.length > 3 ? 3 : filteredGames.length,
+            }}
+            dataSource={filteredGames}
+            renderItem={(item) => (
+              <List.Item>
+                <Skeleton loading={isLoading} active>
+                  <GameCard
+                    title={item.title}
+                    creator={item.creator}
+                    tags={item.tags}
+                  />
+                </Skeleton>
+              </List.Item>
+            )}
+          />
+        </div>
       </div>
     </div>
   );
