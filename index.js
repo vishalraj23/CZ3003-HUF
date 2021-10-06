7f5904e54b5a33a64203143c87548cca84ef5b34
Nickelodeon7856 <72136295+Nickelodeon7856@users.noreply.github.com>
2021-10-06 23:16:14 +0800

    Removed unnecessary Form.item, added default values for correct answer and duration, changed button layout

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import "antd/dist/antd.css";
 import "./index.css";
 import { Form, Input, Button, InputNumber } from "antd";
 
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
 const QuizCreationPage = () => {
   const onFinish = (values) => {
     console.log("Success:", values);
@@ -46,8 +53,8 @@ const QuizCreationPage = () => {
         <Input placeholder="Enter Quiz 1 Description" />
       </Form.Item>
 
-      <Form.Item label="Duration(In Seconds)">
         <Form.Item
+          label="Duration(In Seconds)"
           name="Duration(In Seconds)"
           noStyle
           rules={[
@@ -57,18 +64,16 @@ const QuizCreationPage = () => {
           ]}
         >
           <Input placeholder="Enter the Duration for the game in seconds (The maximum amount of time that you can set the game is: 1 Hour" />
-          <InputNumber min={1} max={3600} /> 
+          <InputNumber min={1} max={3600} defaultValue={1} onChange={onChange}/> 
         </Form.Item>
-        <span className="ant-form-text"></span>
-      </Form.Item>
       
-
       <Form.Item
         label="Question 1"
         name="Question 1"
         rules={[
           {
             required: true,
+            message: 'Please input question!',
           },
         ]}
       >
@@ -81,6 +86,7 @@ const QuizCreationPage = () => {
         rules={[
           {
             required: true,
+            message: 'Empty! Please input first option!',
           },
         ]}
       >
@@ -93,6 +99,7 @@ const QuizCreationPage = () => {
         rules={[
           {
             required: true,
+            message: 'Empty! Please input second option!',
           },
         ]}
       >
@@ -105,6 +112,7 @@ const QuizCreationPage = () => {
         rules={[
           {
             required: true,
+            message: 'Empty! Please input third option!',
           },
         ]}
       >
@@ -117,48 +125,34 @@ const QuizCreationPage = () => {
         rules={[
           {
             required: true,
+            message: 'Empty! Please input fourth option!',
           },
         ]}
       >
         <Input placeholder="Enter Option 4" />
       </Form.Item>
 
-      <Form.Item label="Correct Answer">
         <Form.Item
+          label="Correct Answer"
           name="Correct Answer"
           noStyle
           rules={[
             {
               required: true,
+              message: 'Empty! Please input correct answer for the question!',
             },
           ]}
         >
           <Input placeholder="Choose between options 1 to 4 for the correct answer" />
-          <InputNumber min={1} max={4} /> 
+          <InputNumber min={1} max={4} defaultValue={1} onChange={onChange}/> 
         </Form.Item>
-        <span className="ant-form-text"></span>
-      </Form.Item>
 
-      
-      <Form.Item
-        wrapperCol={{
-          offset: 2,
-          span: 16,
-        }}
-      >
-        <Button htmlType="back">
+      <Form.Item {...tailLayout}>
+      <Button htmlType="button" htmlType="back">
           Back
         </Button>
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="primary" htmlType="next">
-          Next
+        <Button type="primary" htmlType="submit">
+          Submit
         </Button>
       </Form.Item>
 
