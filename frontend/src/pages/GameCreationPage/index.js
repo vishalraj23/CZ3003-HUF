95154f7e6cdfb4532f855d44d92c52655d3cec51
Chi hui <chihui8199@gmail.com>
2021-10-11 00:59:27 +0800

    remove unused import

--- frontend/src/pages/GameCreationPage/index.js
+++ frontend/src/pages/GameCreationPage/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import "antd/dist/antd.css";
 import "./index.css";
 import { Tag, Tooltip, Form, Input, Button, InputNumber } from "antd";
